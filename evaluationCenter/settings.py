e4a6e75a657a14e18fd9b251758fc88160e92363
HoangNguyen <84216736+hoangnc1709@users.noreply.github.com>
2023-03-31 20:56:48 +0700

    Fix cors (#14)
    Co-authored-by: nchoangfossil <nchoang@fossil.com>

--- evaluationCenter/settings.py
+++ evaluationCenter/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "rest_framework",
+    "corsheaders",
     "evaluation",
 ]
 
@@ -34,6 +35,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "evaluationCenter.urls"
@@ -103,3 +106,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ORIGIN_ALLOW_ALL=True
