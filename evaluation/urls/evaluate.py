06a1d55cfa73fcfee9004d12f3698b48068e6a3b
Anh Nguyen <ngvanhfw@gmail.com>
2023-04-07 14:32:35 +0700

    demop

--- evaluation/urls/evaluate.py
+++ evaluation/urls/evaluate.py
@@ -6,6 +6,10 @@ from evaluation.views.evaluate import EvaluateView
 urlpatterns_evaluate = [
     path('evaluate', EvaluateView.evaluate),
     path('evaluate/compare', EvaluateView.compare),
+    path('evaluate/selling_as_is', EvaluateView.evaluate1),
+    path('evaluate/selling_to_be', EvaluateView.evaluate2),
+    path('evaluate/rescheduling_as_is', EvaluateView.evaluate3),
+    path('evaluate/rescheduling_to_be', EvaluateView.evaluate4),
 ]
 
 urlpatterns_evaluate = format_suffix_patterns(urlpatterns_evaluate)
--- evaluation/views/evaluate.py
+++ evaluation/views/evaluate.py
@@ -17,3 +17,196 @@ class EvaluateView:
         result = Compare.compare(body)
         json_response = result.__dict__
         return JsonResponse(json_response, safe=False)
+
+    @staticmethod
+    @api_view(['GET'])
+    def evaluate1(request):
+        result = [
+            {
+                "name": "Atadi",
+                "totalCycleTime": 110.425,
+                "totalCost": 8.834,
+                "unitCost": [
+                    {
+                        "lane": "Customer Service Department",
+                        "cost": 0.08
+                    },
+                    {
+                        "lane": "Finance Department",
+                        "cost": 0.08
+                    }
+                ],
+                "transparency": [
+                    {
+                        "view": "Customer Service Department View",
+                        "numberOfExplicitTask": 4,
+                        "transparency": 0.308
+                    },
+                    {
+                        "view": "Finance Department View",
+                        "numberOfExplicitTask": 4,
+                        "transparency": 0.308
+                    },
+                    {
+                        "view": "Customer View",
+                        "numberOfExplicitTask": 5,
+                        "transparency": 0.384
+                    }
+                ],
+                "totalNumberExplicitTasks": 13,
+                "quality": 0.76,
+                "numberOfOptionalTasks": 3,
+                "totalTasks": 13,
+                "flexibility": 0.2307,
+                "handledTasks": 0,
+                "unHandledTasks": 0,
+                "exceptionHandling": 1
+            }
+        ]
+
+        return JsonResponse(result, safe=False)
+
+    @ staticmethod
+    @ api_view(['GET'])
+    def evaluate2(request):
+        result = [
+            {
+                "name": "Atadi",
+                "totalCycleTime": 59.225,
+                "totalCost": 4.378,
+                "unitCost": [
+                    {
+                        "lane": "Customer Service Department",
+                        "cost": 0.08
+                    },
+                    {
+                        "lane": "Finance Department",
+                        "cost": 0.08
+                    }
+                ],
+                "transparency": [
+                    {
+                        "view": "Customer Service Department View",
+                        "numberOfExplicitTask": 4,
+                        "transparency": 0.333
+                    },
+                    {
+                        "view": "Finance Department View",
+                        "numberOfExplicitTask": 3,
+                        "transparency": 0.25
+                    },
+                    {
+                        "view": "Customer View",
+                        "numberOfExplicitTask": 5,
+                        "transparency": 0.417
+                    }
+                ],
+                "totalNumberExplicitTasks": 12,
+                "quality": 0.76,
+                "numberOfOptionalTasks": 2,
+                "totalTasks": 12,
+                "flexibility": 0.166,
+                "handledTasks": 0,
+                "unHandledTasks": 0,
+                "exceptionHandling": 1
+            }
+        ]
+
+        return JsonResponse(result, safe=False)
+
+    @ staticmethod
+    @ api_view(['GET'])
+    def evaluate3(request):
+        result = [
+            {
+                "name": "Atadi",
+                "totalCycleTime": 73.9,
+                "totalCost": 5.921,
+                "unitCost": [
+                    {
+                        "lane": "Customer Service Department",
+                        "cost": 0.08
+                    },
+                    {
+                        "lane": "Finance Department",
+                        "cost": 0.08
+                    }
+                ],
+                "transparency": [
+                    {
+                        "view": "Customer Service Department View",
+                        "numberOfExplicitTask": 4,
+                        "transparency": 0.4
+                    },
+                    {
+                        "view": "Finance Department View",
+                        "numberOfExplicitTask": 3,
+                        "transparency": 0.3
+                    },
+                    {
+                        "view": "Customer View",
+                        "numberOfExplicitTask": 3,
+                        "transparency": 0.3
+                    }
+                ],
+                "totalNumberExplicitTasks": 10,
+                "quality": 0.2,
+                "numberOfOptionalTasks": 8,
+                "totalTasks": 10,
+                "flexibility": 0.8,
+
+                "handledTasks": 0,
+                "unHandledTasks": 0,
+                "exceptionHandling": 1
+            }
+        ]
+
+        return JsonResponse(result, safe=False)
+
+    @ staticmethod
+    @ api_view(['GET'])
+    def evaluate4(request):
+        result = [
+            {
+                "name": "Atadi",
+                "totalCycleTime": 32.5,
+                "totalCost": 2.6,
+                "unitCost": [
+                    {
+                        "lane": "Customer Service Department",
+                        "cost": 0.08
+                    },
+                    {
+                        "lane": "Finance Department",
+                        "cost": 0.08
+                    }
+                ],
+                "transparency": [
+                    {
+                        "view": "Customer Service Department View",
+                        "numberOfExplicitTask": 4,
+                        "transparency": 0.445
+                    },
+                    {
+                        "view": "Finance Department View",
+                        "numberOfExplicitTask": 2,
+                        "transparency": 0.222
+                    },
+                    {
+                        "view": "Customer View",
+                        "numberOfExplicitTask": 3,
+                        "transparency": 0.333
+                    }
+                ],
+                "totalNumberExplicitTasks": 9,
+                "quality": 0.7,
+                "numberOfOptionalTasks": 7,
+                "totalTasks": 9,
+                "flexibility": 0.777,
+                "handledTasks": 0,
+                "unHandledTasks": 0,
+                "exceptionHandling": 1
+            }
+        ]
+
+        return JsonResponse(result, safe=False)
