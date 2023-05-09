9c8690cf573a29406bde7dc33d391ca9488cf992
Anh Nguyen <ngvanhfw@gmail.com>
2023-05-09 18:41:51 +0700

    fix cors

--- bpsky.py
+++ bpsky.py
@@ -3,4 +3,4 @@ from flask_cors import CORS
 
 # Define bpsky.
 bpsky = Flask(__name__, static_url_path="/static/")
-CORS(bpsky, supports_credentials=True, resources=r"/")
+CORS(bpsky, supports_credentials=True, resources={r"/*": {"origins": "*"}})
