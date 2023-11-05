a0579f0f5986ee966cd1320a59a51c8ecb116fcd
iamlvv <levanvy28062002@gmail.com>
2023-11-05 18:19:23 +0700

    delete socket

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from bpsky import bpsky
-from socket_python import socketio
 import os
 
-socketio.init_app(bpsky)
-
 if __name__ == "__main__":
-    socketio.run(bpsky, host="0.0.0.0", port=os.environ.get("PORT", 8000), debug=True)
+    bpsky.run(host="0.0.0.0", port=os.environ.get("PORT", 8000), debug=True)
