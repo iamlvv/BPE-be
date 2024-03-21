fb34e12ee0c98833dd32fccca7ee7565aac70b19
iamlvv <levanvy28062002@gmail.com>
2024-03-21 10:11:31 +0700

    fix: api close survey params to body

--- controller/survey_features_controllers/response.py
+++ controller/survey_features_controllers/response.py
@@ -58,7 +58,8 @@ def get_survey_response():
 
 @bpsky.route("/api/v1/survey/publish/close", methods=["POST"])
 def close_publish_survey():
-    process_version_version = request.args.get("processVersionVersion", None)
+    body = load_request_body(request)
+    process_version_version = body["processVersionVersion"]
     data = Survey_service.close_publish_survey(process_version_version)
     return bpsky.response_class(
         response=jsonpickle.encode(data, unpicklable=False),
