725ee9a452ee888571b1aba03b43ff2c7a69cf5c
iamlvv <levanvy28062002@gmail.com>
2024-03-27 21:20:00 +0700

    update: requirements

---
+++ services/survey_service/cron/scheduling.py
@@ -0,0 +1,14 @@
+# from crontab import CronTab
+#
+#
+# def hello_world():
+#     print("Hello World!")
+#
+#
+# cron = CronTab(user="root")
+# job = cron.new(
+#     command="python /bpe-be/services/survey_service/cron/scheduling.py hello_world",
+#     comment="Hello World",
+# )
+# job.minute.every(1)
+# cron.write()
